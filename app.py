bd021e6244e414c4776d1183ebca9af88467e304
Alec Sharp <alecsharpie@gmail.com>
2022-01-26 22:42:48 +1100

    format result header and display selected jpg in sidebar

--- app.py
+++ app.py
@@ -14,12 +14,18 @@ labels = [
 ]
 st.header("Cassava Disease Identifier")
 
+st.markdown("""---""")
+
 jpg = st.sidebar.file_uploader(
     "Upload a picture of one of your cassava leaves", type=([".jpg", '.png']))
 
-demo = st.sidebar.checkbox('use demo image', value=True)
-if demo:
-    image = Image.open('images/demo.jpg')
+if not jpg:
+    demo = st.sidebar.radio("Use a demo image", ('Healthy Demo', 'Unhealthy Demo'))
+
+    if demo == 'Unhealthy Demo':
+        image = Image.open('images/unhealthy_demo.jpg')
+    elif demo == 'Healthy Demo':
+        image = Image.open('images/healthy_demo.jpg')
 
     img_byte_arr = io.BytesIO()
     image.save(img_byte_arr, format='PNG')
@@ -27,18 +33,18 @@ if demo:
     jpg = img_byte_arr
 
 if jpg:
+    st.sidebar.image(jpg, caption = 'Current Selection')
 
+if jpg:
     url = "http://127.0.0.1:8000/predict"  # local
     #url = "https://idc-mvds5dflqq-ew.a.run.app/annotate"  # production
     files = {"file": ('image_to_identify.jpg', jpg, "multipart/form-data")}
     response = requests.post(url, files=files).json()
-
-    st.write('Results')
     if response['healthy'] > 0.5:
-        new_title = '<p style="color:Green; font-size: 24px;">Healthy Cassava</p>'
+        new_title = '<span style="color:White; font-size: 24px;">Result:   </span><span style="color:Green; font-size: 26px;">Healthy Cassava</span>'
         st.markdown(new_title, unsafe_allow_html=True)
     else:
-        new_title = '<p style="color:Red; font-size: 24px;">Sick Cassava</p>'
+        new_title = '<span style="color:White; font-size: 24px;">Result:   </span><span style="color:Red; font-size: 26px;">Sick Cassava</span>'
         st.markdown(new_title, unsafe_allow_html=True)
 
     cols = st.columns(4)
@@ -52,13 +58,11 @@ if jpg:
     predictions_sort = sorted(predictions, reverse=True)
     for col, pred, title, label in zip(cols, predictions_sort, titles_sort,
                                        labels_sort):
-        col.write(title)
         col.markdown(
             f"<p style='color:White; font-size: 24px;'>{str(round(pred * 100, 1))}%</p>",
             unsafe_allow_html=True)
-        col.image(f'images/{label}.jpg')
-        #new_title = '<p style="font-family:sans-serif; color:Green; font-size: 42px;">New image</p>'
-        #col.markdown(new_title, unsafe_allow_html=True)
+        col.write(title)
+        col.image(f'images/{label}.jpg', caption = f"Example of {title}")
 
 else:
     response = {
